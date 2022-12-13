8833cd96a2083d5b4361b17773a59cfabe4ead39
BFDavidGamboa <david.gamboa@blankfactor.com>
2022-12-13 15:05:35 -0500

    Feat: added reading of body, response error if the json format is incorrect, also new error in case of unmarshall incorrect. Added oauth from bookstore_oauth-api implementation and error managment for them

--- controllers/items_controller.go
+++ controllers/items_controller.go
@@ -1,13 +1,16 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/BFDavidGamboa/bookstore_items-api/domain/items"
 	"github.com/BFDavidGamboa/bookstore_items-api/services"
 	"github.com/BFDavidGamboa/bookstore_items-api/utils/http_utils"
 	"github.com/BFDavidGamboa/bookstore_oauth-go/oauth"
+	"github.com/BFDavidGamboa/bookstore_utils-go/rest_errors"
 )
 
 var (
@@ -28,13 +31,26 @@ func (c *itemsController) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	item := items.Item{
-		Seller: oauth.GetCallerID(r),
+	requestBoody, err := io.ReadAll(r.Body)
+	if err != nil {
+		respErr := rest_errors.NewBadRequestError("invalid request body")
+		http_utils.RespondError(w, respErr)
+		return
+	}
+	defer r.Body.Close()
+
+	var itemRequest items.Item
+	if err := json.Unmarshal(requestBoody, &itemRequest); err != nil {
+		respErr := rest_errors.NewBadRequestError("invalid item json  body")
+		http_utils.RespondError(w, respErr)
+		return
 	}
 
-	result, err := services.ItemsService.Create(item)
-	if err != nil {
-		http_utils.RespondError(w, err)
+	itemRequest.Seller = oauth.GetClientId(r)
+
+	result, createErr := services.ItemsService.Create(itemRequest)
+	if createErr != nil {
+		http_utils.RespondError(w, createErr)
 		return
 	}
 	http_utils.RespondJson(w, http.StatusCreated, result)
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.19
 require github.com/BFDavidGamboa/bookstore_oauth-go v0.0.0-20221213171400-43f8a2d0f76d
 
 require (
-	github.com/BFDavidGamboa/bookstore_utils-go v0.0.0-20221212214058-2429647a74ec // indirect
+	github.com/BFDavidGamboa/bookstore_utils-go v0.0.0-20221212214058-2429647a74ec
 	github.com/gorilla/mux v1.8.0
 	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0 // indirect
 )
